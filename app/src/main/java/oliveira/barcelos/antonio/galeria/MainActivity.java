d0dde40303eeabacf22e28ad47fd3245851a8817
20211TIIMI0117 <pedroantoiocec@gmail.com>
2023-04-05 12:40:25 -0300

    Initial commit

---
+++ app/src/main/java/oliveira/barcelos/antonio/galeria/MainActivity.java
@@ -0,0 +1,14 @@
+package oliveira.barcelos.antonio.galeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
